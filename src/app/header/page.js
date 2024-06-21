2f7b30638fe0762c9d65cbc49d8768d4bbef5008
Airton Sales <airtonsales0@gmail.com>
2024-06-21 02:11:44 -0300

    teste 3

--- src/app/header/page.js
+++ src/app/header/page.js
@@ -10,21 +10,21 @@ export default function Header() {
   };
 
   return (
-     <header className={`w-[375px] h-[72px] bg-white flex justify-center ${menuActive ? "flex-col h-[325px] items-center" : "justify-center"}`}>
-      <div className="flex justify-between items-center gap-36">
-        <Image src="/logo.svg" alt="Logo" width={170} height={16} />
-        <button className="block md:hidden" onClick={toggleMenu}>
+     <header className={`w-screen h-[72px] bg-white flex justify-center ${menuActive ? "flex-col h-[330px] absolute top-0 left-0 right-0 items-center" : "justify-center"}`}>
+      <div className="flex justify-between  w-screen items-center gap-36">
+        <Image className="ml-5" src="/logo.svg" alt="Logo" width={170} height={16} />
+        <button className="block md:hidden mr-5" onClick={toggleMenu}>
             <Image src="/Menu.svg" alt="Menu" width={20} height={6} />
         </button>
       </div>
-      <nav className={`mt-10 ${menuActive ? "block" : "hidden"}`}>
-        <ul className="flex flex-col gap-1 items-center">
+      <nav className={`mt-16 w-screen flex flex-col items-center ${menuActive ? "block" : "hidden"}`}>
+        <ul className="flex flex-col gap-3 items-center">
           <li>STORIES</li>
           <li>FEATURES</li>
           <li>PRICING</li>
         </ul>
-        <div className="w-[310px] h-[1px] bg-[#979797] mt-10"></div>
-        <button className="w-[310px] h-[48px] bg-black text-white -tracking-[-3px] font-bold mt-10">GET AN INVITE</button>
+        <div className="w-[86%] h-[1px] bg-[#97979783] mt-5"></div>
+        <button className="w-[86%] h-[48px] bg-black text-white -tracking-[-3px] font-bold mt-5">GET AN INVITE</button>
       </nav>
     </header>
   );
--- src/app/home/page.js
+++ src/app/home/page.js
@@ -3,7 +3,16 @@ import Image from "next/image";
 export default function Home() {
 
 
-    return <main>
-        <Image src="/Bitmap.svg" alt="Home" width={375} height={375} />
-    </main>;
+    return (
+    <main>
+        <Image src="/Bitmap.svg" alt="Home" width={500} height={500} />
+        <div className="w-[500px] h-[419px] flex justify-center items-center bg-black">
+            <div className="bg-black w-[318px] h-[275px]">
+                <h1 className="text-white font-bold text-[32px] text-left">Create and share your photo stories.</h1>
+                <p className="text-white text-[15px] text-left mt-5">Photosnap is a platform for photographers and visual storytellers. We make it easy to share photos, tell stories and connect with others.</p>
+                <p className="text-white text-[12px] text-left mt-12 flex gap-5">GET AN INVITE <Image src="/arrow.svg" alt="Arrow" width={41} height={1} /></p>
+            </div>
+        </div>
+    </main>
+    );
 }
